1a28dcf039a6fb691828cdb7905bf8c4541710d8
Cloudiu9 <claudiubordea5@gmail.com>
2026-09-07 20:18:31 +0300

    Finished job component and mobile optimization.

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,11 +1,5 @@
 import Job from "@/components/Job";
 
 export default function page() {
-  return (
-    // TODO list out all jobs from data.json
-    <div>
-      <Job />
-      <Job />
-    </div>
-  );
+  return <Job />;
 }
--- components/Job.tsx
+++ components/Job.tsx
@@ -1,57 +1,84 @@
 import Image from "next/image";
+import data from "../data.json";
 
-// TODO convert this with dynamic data from data.json
-
-export default function Job() {
+export default async function Job() {
   return (
-    // TODO only add border-l-main border-l-4 if posting has featured flag
-    <div className="flex justify-between items-center bg-white p-6 mb-4 shadow-lg rounded-xl border-l-main border-l-4">
-      {/* main front part grouping */}
-      <div className="flex gap-4">
-        <Image
-          src="photosnap.svg"
-          alt="Company"
-          width={80}
-          height={1}
-          className="object-contain"
-        />
-        <div className="flex flex-col gap-1">
-          <div className="flex gap-2 text-lg">
-            <strong id="company" className="text-main/80">
-              Photosnap
-            </strong>
-            <div className="flex items-center gap-2 text-white font-bold">
-              <p className="py-0.5 px-2 rounded-xl text-[12px] bg-main">NEW!</p>
-              <p className="py-0.5 px-2 rounded-xl text-[12px] bg-black">
-                FEATURED
-              </p>
+    <div className="md:p-4">
+      {data.map((job, idx) => (
+        <div
+          key={idx}
+          className="flex flex-col md:flex-row md:justify-between md:items-center bg-white py-8 px-8 md:p-6 mt-4 mb-12 md:mb-4 shadow-lg rounded-xl border-l-main border-l-4 relative"
+        >
+          {/* main front part grouping */}
+          <div className="flex flex-col md:flex-row gap-4">
+            <Image
+              src={job.logo}
+              alt={job.company}
+              width={80}
+              height={1}
+              className="object-contain absolute -top-8 md:static w-15 md:w-17"
+            />
+            <div className="flex flex-col gap-1">
+              <div className="flex gap-2 text-lg">
+                <strong id="company" className="text-main/80">
+                  {job.company}
+                </strong>
+                <div className="flex items-center gap-2 text-white font-bold">
+                  {job.new && (
+                    <p className="py-0.5 px-2 rounded-xl text-[12px] bg-main">
+                      NEW!
+                    </p>
+                  )}
+                  {job.featured && (
+                    <p className="py-0.5 px-2 rounded-xl text-[12px] bg-black">
+                      FEATURED
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              <h2
+                id="position"
+                className="text-black font-bold text-lg hover:cursor-pointer hover:text-main"
+              >
+                {job.position}
+              </h2>
+
+              <div className="flex gap-3 text-sm font-semibold text-gray-400">
+                <p>{job.postedAt}</p>·<p>{job.contract}</p>·
+                <p>{job.location}</p>
+              </div>
             </div>
           </div>
+          {/* mobile divider */}
+          <hr className="md:hidden my-3 w-full" />
 
-          <h2 id="position" className="text-black font-bold text-lg">
-            Senior Frontend Developer
-          </h2>
+          {/* end part grouping */}
+          <div className="flex flex-wrap gap-2 items-center text-sm font-bold text-main ">
+            <div
+              className="py-1 px-2 bg-page-bg hover:text-white hover:cursor-pointer hover:bg-main"
+              id="role"
+            >
+              <span>{job.role}</span>
+            </div>
+            <div
+              className="py-1 px-2 bg-page-bg hover:text-white hover:cursor-pointer hover:bg-main"
+              id="level"
+            >
+              <span>{job.level}</span>
+            </div>
 
-          <div className="flex gap-3 text-sm font-semibold text-gray-400">
-            <p>1d ago</p>·<p>Full Time</p>·<p>USA only</p>
+            {job.languages.map((language, idx) => (
+              <span
+                key={idx}
+                className="py-1 px-2 bg-page-bg hover:text-white hover:cursor-pointer hover:bg-main"
+              >
+                {language}
+              </span>
+            ))}
           </div>
         </div>
-      </div>
-
-      {/* end part grouping */}
-      <div className="flex gap-2 items-center text-sm font-semibold text-main">
-        <div className="py-1 px-2 bg-page-bg" id="role">
-          <span>Frontend</span>
-        </div>
-        <div className="py-1 px-2 bg-page-bg" id="level">
-          <span>Senior</span>
-        </div>
-        {/* will be an array */}
-        {/* extra div here to separate languages? */}
-        <span className="py-1 px-2 bg-page-bg">HTML</span>
-        <span className="py-1 px-2 bg-page-bg">CSS</span>
-        <span className="py-1 px-2 bg-page-bg">JavaScript</span>
-      </div>
+      ))}
     </div>
   );
 }
