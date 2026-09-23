c5978571287d77948ba072f55153f21d62d330ff
Cloudiu9 <claudiubordea5@gmail.com>
2026-09-23 12:30:48 +0300

    Added favorites e2e test.

--- tests/e2e/job.spec.ts
+++ tests/e2e/job.spec.ts
@@ -34,3 +34,48 @@ test("homepage filters job listings", async ({ page }) => {
     page.getByRole("button", { name: "Senior Frontend Developer" }),
   ).toBeVisible();
 });
+
+test("user can favorite a job", async ({ page }) => {
+  await page.goto("/");
+
+  // user clicks on first two jobs
+  await page.getByRole("button", { name: "Senior Frontend Developer" }).click();
+  await page.getByRole("button", { name: "Fullstack Developer" }).click();
+
+  // heart shows up
+  await expect(
+    page.getByRole("button", { name: "Senior Frontend Developer💖" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Fullstack Developer💖" }),
+  ).toBeVisible();
+
+  // navigate to profile page
+  await page.getByRole("link", { name: "Profile" }).click();
+
+  // user removes first job
+  await page
+    .getByRole("button", { name: "Senior Frontend Developer💖" })
+    .click();
+
+  // first job removed, second still there
+  await expect(
+    page.getByRole("button", { name: "Senior Frontend Developer💖" }),
+  ).not.toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Fullstack Developer💖" }),
+  ).toBeVisible();
+
+  // check persistance on refresh
+  await page.reload();
+  await expect(
+    page.getByRole("button", { name: "Fullstack Developer💖" }),
+  ).toBeVisible();
+
+  // clear all button works
+  await page.getByRole("button", { name: "Clear Favorites" }).click();
+
+  await expect(
+    page.getByRole("button", { name: "Fullstack Developer💖" }),
+  ).not.toBeVisible();
+});
